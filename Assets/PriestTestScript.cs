4c5a45052be032884233ef910fb168e412437653
luismb55 <luismb55@gmail.com>
2024-01-27 15:14:26 +0100

    Update PriestTestScript.cs

--- Assets/PriestTestScript.cs
+++ Assets/PriestTestScript.cs
@@ -7,6 +7,7 @@ public class PriestTestScript : MonoBehaviour
     [SerializeField] public bool throwBaby;
     [SerializeField] public  float aimForce;
 
+    [SerializeField] private bool _ready;
     [SerializeField] private bool _releaseBaby;
     private Animator _animator;
 
@@ -16,9 +17,10 @@ public class PriestTestScript : MonoBehaviour
     void Start()
     {
         _animator = GetComponent<Animator>();
-        _aimForce = 0;
-        _releaseBaby = false;
         throwBaby = false;
+        aimForce = 0;
+        _releaseBaby = false;
+        _ready = false;
     }
 
     // Update is called once per frame
@@ -26,24 +28,30 @@ public class PriestTestScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            _aimForce = 0.3f;
+            aimForce = 0.3f;
             Debug.Log("Force Strong");
         } else if (Input.GetKey(KeyCode.RightArrow))
         {
-            _aimForce = 0.7f;
+            aimForce = 0.7f;
             Debug.Log("Force Mid");
         } else if (Input.GetKey(KeyCode.DownArrow))
         {
-            _aimForce = 1.0f;
+            aimForce = 1.0f;
             Debug.Log("Force Low");
         }
         else
         {
-            if(_aimForce > 0)
+            if(aimForce > 0)
                 ThrowBaby();
-            _aimForce = 0;
+            aimForce = 0;
         }
-        _animator.SetFloat("AimForce", _aimForce);
+        _animator.SetFloat("AimForce", aimForce);
+    }
+
+    void ReloadBaby()
+    {
+        _ready = true;
+        Debug.Log("Reloaded!");
     }
 
     void ThrowBaby()
@@ -57,5 +65,9 @@ public class PriestTestScript : MonoBehaviour
     bool BabyReleased()
     {
         return _releaseBaby;
+    }
+    bool PriestReady()
+    {
+        return _ready;
     }
 }
